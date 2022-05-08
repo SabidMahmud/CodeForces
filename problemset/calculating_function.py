3b32bff69257e277e5185d6a10e265aeec357813
SabidMahmud <sabidmahmud96@gmail.com>
2022-05-08 15:15:04 +0600

    Calculating Function solved

--- problemset/calculating_function.py
+++ problemset/calculating_function.py
@@ -1,15 +1,11 @@
-# def f( n ) :
-    # result = 0
-    # counter = 0
-    # while counter < n:
-        # result += ((-1) ** n) * n
-        # counter += 1
 
-    # return result
 
 n = int( input() )
 result = 0
-for i in range(1,n+1) :
-    result = result + ((-1)**i)*i
 
-print( result )
+if n%2 == 0 :
+    result = ( n/2 )
+else :
+    result = -(n + 1)/2
+
+print( int(result) )
